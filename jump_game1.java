e5ab9131690b3c17d574d834ea9de31305198f4a
Divyanshu-rajpoot <divy.r.007@gmail.com>
2024-07-22 21:15:55 +0530

    Jump game 1 best approach

---
+++ jump_game1.java
@@ -0,0 +1,21 @@
+/**
+ * jump_game1
+ */
+//55. Jump Game
+public class jump_game1 {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,1,4};
+
+    }
+    public boolean canJump(int[] nums) {
+        //using greeady approach
+        int target = nums.length-1;
+        for(int i = nums.length-1 ; i>=0 ;i--){
+            if(i + nums[i] >= target){
+                target = i;
+            }
+        }
+        return target == 0;
+    }
+    
+}
