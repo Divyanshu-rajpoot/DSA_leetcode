64980da17922325cc8713d6c0dcf5475a0d2c702
Divyanshu-rajpoot <divy.r.007@gmail.com>
2024-11-16 18:23:38 +0530

    more added

---
+++ Contiguous_Array.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Contiguous_Array {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 1, 1, 0, 1, 1, 1, 0, 1};
+        int sum = 0;
+        int[] prefix = new int[arr.length];
+        prefix[0] = sum;
+        for (int i = 1; i < prefix.length; i++) {
+            if()
+        }
+
+
+
+
+        // System.out.println(size);
+    }
+}
--- recus/integer_replace.java
+++ recus/integer_replace.java
@@ -2,6 +2,19 @@ package recus;
 
 public class integer_replace {
     public static void main(String[] args) {
+        int num = 15;
+        int[] arr = new int[num+1];
+        System.out.println(replace(num,arr));
+    }
+    //simple resusive solution but gives tle in larger test cases
+    public static int replace(int num , int[] arr){
+        if(num == 1){
+            return 0;
+        }
         
+        if(num % 2 ==0){
+            return 1+replace(num/2);
+        }
+        return 1+Math.min(replace(num+1) , replace(num-1));
     }
 }
